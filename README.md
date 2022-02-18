873aad44c300c4bf049cfdf35208a0304e18fa17
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-19 02:26:42 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Coffee shop
+In this project I will create a coffee shop management system with two category of user availability. I will focus on the backend system most. 
+---
+The technologies, I will use for this project:
+1. EJS  
+2. SCSS
+3. Javascript (client-side)
+4. Nodejs (Server-side)
+5. Express
+6. MongoDB
+7. Mongoose
+
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const {MONGODB_CONNECTION_URI} = process.env;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(MONGODB_CONNECTION_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+        console.info("Connected to the Database");
+		app.listen(PORT, () => {
+			console.log(`Server running at http://localhost:${PORT}`);
+		});
+	})
+	.catch((err) => console.error(err));
