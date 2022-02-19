dad2555d8f382c9bf7062020449dc9eea83ab721
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-19 19:56:48 +0600

    search bar added and fixed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,11 +2,24 @@ window.onload = () => {
 	const verticalMenu = document.querySelector(".vertical-menu");
 	const revealList = document.querySelector(".bi-list");
 	const closeMenu = document.querySelector(".bi-x-circle-fill");
+	const searchIcon = document.getElementById("search-icon");
+	const searchBox = document.querySelector(".search-input");
 	closeMenu.addEventListener("click", function () {
 		verticalMenu.classList.remove("reveal-sidebar");
 	});
 	revealList.addEventListener("click", function () {
 		verticalMenu.classList.add("reveal-sidebar");
+	});
+	searchIcon.addEventListener("click", function () {
+		this.style.display = "none";
+		searchBox.classList.add("search-scaling");
+		let closeBtn = this.nextElementSibling;
+		closeBtn.classList.remove("set-invisible");
+		closeBtn.addEventListener("click", function () {
+			this.classList.add("set-invisible");
+			searchIcon.style.display = "block";
+			searchBox.classList.remove("search-scaling");
+		});
 	});
 };
 
