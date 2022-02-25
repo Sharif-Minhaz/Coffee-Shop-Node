245dd7f8168576956f4641c9b8f4cd160b25c074
Minhaz <sharif35-3001@diu.edu.bd>
2022-02-25 11:26:22 +0600

    profile related route and controller added

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,11 +1,89 @@
-exports.createProfileGetController = (req, res, next) => {
-	res.render("pages/profile/create-profile", {
-		title: "Coffee Shop | Create Profile",
-		flashMessage: {},
-		errors: {},
-	});
+const Profile = require("../models/Profile.model");
+const Flash = require("../utils/Flash");
+const { validationResult } = require("express-validator");
+const errorFormatter = require("../utils/validatorErrorFormatter");
+
+exports.createProfileGetController = async (req, res, next) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user._id });
+		if (profile) {
+			return res.redirect("/profile/show-profile");
+		}
+		res.render("pages/profile/create-profile", {
+			title: "Coffee Shop | Create Profile",
+			flashMessage: {},
+			errors: {},
+			values: {},
+		});
+	} catch (err) {
+		next(err);
+	}
 };
 
 exports.createProfilePostController = async (req, res, next) => {
+	const { name, email, title, bio, website } = req.body;
+
+	const errors = validationResult(req).formatWith(errorFormatter);
+	if (!errors.isEmpty()) {
+		req.flash("fail", "Please check your fields");
+		return res.render("pages/profile/create-profile", {
+			title: "Coffee Shop | Create Profile",
+			flashMessage: Flash.getMessage(req),
+			errors: errors.mapped(),
+			values: req.body,
+		});
+	}
+
+	const profile = new Profile({
+		user: req.user._id,
+		name,
+		email,
+		title,
+		bio,
+		website: website || "",
+	});
+	try {
+		await profile.save();
+		req.flash("Success", "Profile updated successfully");
+		return res.redirect("/profile/show-profile");
+	} catch (err) {
+		next(err);
+	}
 	res.send("Profile");
-}
+};
+
+exports.profileGetController = async (req, res, next) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user._id });
+		if (!profile) {
+			return res.redirect("/profile/create-profile");
+		}
+		res.render("pages/profile/show-profile", {
+			title: "Coffee Shop | Profile",
+			errors: {},
+			flashMessage: Flash.getMessage(req),
+			values: {},
+			profile,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+exports.updateProfileGetController = async (req, res, next) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user._id });
+		if (!profile) {
+			return res.redirect("/profile/create-profile");
+		}
+		res.render("pages/profile/edit-profile", {
+			title: "Coffee Shop | Edit Profile",
+			errors: {},
+			flashMessage: {},
+			values: {},
+			profile,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
