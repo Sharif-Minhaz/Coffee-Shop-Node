e800ce71f24ca10d7f07d8d810c113099a7d166c
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-03-22 17:19:40 +0600

    nodemailer implemented wiht google Oauth2

--- utils/notification.js
+++ utils/notification.js
@@ -1,9 +1,18 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+// creating a google oAuth2 for secured mail sending
+const OAuth2 = google.auth.OAuth2;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken, {
 	lazyLoading: true,
 });
+// gmail notification system with nodemailer
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const refreshToken = process.env.REFRESH_TOKEN;
+const oAuth2_client = new OAuth2(clientId, clientSecret);
+oAuth2_client.setCredentials({ refresh_token: refreshToken });
 
 exports.sendNotification = async (msgBody, toNum, toMail) => {
 	try {
@@ -15,21 +24,29 @@ exports.sendNotification = async (msgBody, toNum, toMail) => {
 		});
 
 		// gmail notification system with nodemailer
+		accessToken = oAuth2_client.getAccessToken();
 		const transporter = nodemailer.createTransport({
 			service: "gmail",
 			auth: {
-				user: "phonixgaruda2021@gmail.com",
+				type: "OAuth2",
+				user: process.env.USER,
 				pass: process.env.EMAIL_PASS,
+				clientId,
+				clientSecret,
+				refreshToken,
+				accessToken,
 			},
 		});
 
 		const mailOptions = {
-			from: "phonixgaruda2021@gmail.com",
+			from: `CAFE <${process.env.USER}>`,
 			to: toMail,
 			subject: "About checkout",
 			text: msgBody,
 		};
 		await transporter.sendMail(mailOptions);
+		transporter.close();
+		
 	} catch (err) {
 		console.error(err);
 	}
