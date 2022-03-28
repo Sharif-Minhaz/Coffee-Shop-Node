26fae2be4c0140b4251127cc583e1dfebd950381
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-03-28 18:36:05 +0600

    initialized blog pages

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -4,6 +4,14 @@ const Post = require("../models/Post.model");
 const { validationResult } = require("express-validator");
 const errorFormatter = require("../utils/validatorErrorFormatter");
 
+exports.allBlogsGetController = async (req, res, next) => {
+	res.render("pages/blogs/show-blogs", {
+		title: "All Blogs",
+		flashMessage: {},
+		orders: await gettingAllOrder(req, next),
+	});
+};
+
 exports.createBlogGetController = async (req, res, next) => {
 	res.render("pages/blogs/create-new-blog", {
 		title: "Create a New Blog",
@@ -14,7 +22,18 @@ exports.createBlogGetController = async (req, res, next) => {
 
 exports.createBlogPostPostController = async (req, res, next) => {
 	const { title, body } = req.body;
+	console.log(req.body);
 	let thumbnail = req.file ? req.file.filename : "";
+	let errors = validationResult(req).formatWith(errorFormatter);
+	console.log(errors);
+	if (!errors.isEmpty()) {
+		return res.render("pages/blogs/create-new-blog", {
+			title: "Create a New Blog",
+			flashMessage: Flash.getMessage(req),
+			orders: await gettingAllOrder(req, next),
+			errors,
+		});
+	}
 	let blogPost = new Post({
 		title,
 		body,
@@ -29,4 +48,3 @@ exports.createBlogPostPostController = async (req, res, next) => {
 		next(err);
 	}
 };
-
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -2,14 +2,18 @@ const router = require("express").Router();
 const {
 	createBlogGetController,
 	createBlogPostPostController,
+	allBlogsGetController,
 } = require("../controllers/blog.controller");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
+const blogValidator = require("../validator/blog.validator");
 const upload = require("../middlewares/upload.middleware");
 
+router.get("/", allBlogsGetController);
 router.get("/create", isAuthenticated, createBlogGetController);
 router.post(
 	"/create",
 	isAuthenticated,
+	blogValidator,
 	upload.single("upload-thumbnail"),
 	createBlogPostPostController
 );
