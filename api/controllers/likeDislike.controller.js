7af1bce30c0b7fa5920670c95c0a345127155015
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-03-31 11:34:32 +0600

    like comment reply setup done

--- api/controllers/likeDislike.controller.js
+++ api/controllers/likeDislike.controller.js
@@ -1,5 +1,84 @@
-var hello = () => {
-	console.log(this);
+const Post = require("../../models/Post.model");
+
+exports.likeGetController = async (req, res, next) => {
+	let { postId } = req.params;
+	let liked = null;
+
+	if (!req.user) {
+		return res.status(403).json({
+			error: "You are not an authenticated user!",
+		});
+	}
+
+	let userId = req.user._id;
+
+	try {
+		let post = await Post.findById(postId);
+		console.log(post);
+		if (post.dislikes.includes(userId)) {
+			await Post.findOneAndUpdate({ _id: postId }, { $pull: { dislikes: userId } });
+			console.log("find and pull the delete");
+		}
+
+		if (post.likes.includes(userId)) {
+			await Post.findOneAndUpdate({ _id: postId }, { $pull: { likes: userId } });
+			liked = false;
+			console.log("unlike");
+		} else {
+			await Post.findOneAndUpdate({ _id: postId }, { $push: { likes: userId } });
+			liked = true;
+			console.log("like");
+		}
+
+		let updatedPost = await Post.findById(postId);
+		console.log(updatedPost);
+		res.status(200).json({
+			liked,
+			totalLikes: updatedPost.likes.length,
+			totalDislikes: updatedPost.dislikes.length,
+		});
+	} catch (err) {
+		res.status(500).json({
+			error: "Server error occurred!",
+		});
+	}
 };
 
-hello.bind(this);
+exports.dislikeGetController = async (req, res, next) => {
+	let { postId } = req.params;
+	let disliked = null;
+
+	if (!req.user) {
+		return res.status(403).json({
+			error: "You are not an authenticated user!",
+		});
+	}
+
+	let userId = req.user._id;
+
+	try {
+		let post = await Post.findById(postId);
+
+		if (post.likes.includes(userId)) {
+			await Post.findOneAndUpdate({ _id: postId }, { $pull: { likes: userId } });
+		}
+
+		if (post.dislikes.includes(userId)) {
+			await Post.findOneAndUpdate({ _id: postId }, { $pull: { dislikes: userId } });
+
+			disliked = false;
+		} else {
+			await Post.findOneAndUpdate({ _id: postId }, { $push: { dislikes: userId } });
+
+			disliked = true;
+		}
+		let updatedPost = await Post.findById(postId);
+		res.status(200).json({
+			disliked,
+			totalLikes: updatedPost.likes.length,
+			totalDislikes: updatedPost.dislikes.length,
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
---
+++ public/scripts/singlePage.js
@@ -0,0 +1,203 @@
+window.onload = function () {
+	const bookmarks = document.getElementsByClassName("bookmark");
+	const likeBtn = document.getElementById("likeBtn");
+	const dislikeBtn = document.getElementById("dislikeBtn");
+	const comment = document.getElementById("comment");
+	const commentHolder = document.getElementById("comment-holder");
+	const reply = document.getElementById("reply");
+
+	[...bookmarks].forEach((bookmark) => {
+		bookmark.style.cursor = "pointer";
+		bookmark.addEventListener("click", function (e) {
+			let target = e.target.parentElement;
+			let headers = new Headers();
+			headers.append("Accept", "Application/JSON");
+
+			let req = new Request(`/api/bookmarks/${target.dataset.post}`, {
+				method: "GET",
+				headers,
+				mode: "cors",
+			});
+
+			fetch(req)
+				.then((res) => res.json())
+				.then((data) => {
+					if (data.bookmark) {
+						target.innerHTML = '<i class="fas fa-bookmark"></i>';
+					} else {
+						target.innerHTML = '<i class="far fa-bookmark"></i>';
+					}
+				})
+				.catch((e) => {
+					alert(e.response.data.error);
+				});
+		});
+	});
+
+	comment.addEventListener("keypress", function (e) {
+		if (e.key === "Enter") {
+			if (e.target.value) {
+				let postId = comment.dataset.post;
+				let data = {
+					body: e.target.value,
+					main: e.target.value,
+				};
+				let req = generateRequest(`/api/comments/${postId}/`, "POST", data);
+				fetch(req)
+					.then((res) => res.json())
+					.then((data) => {
+						let commentElement = createComment(data);
+						commentHolder.insertBefore(commentElement, commentHolder.children[0]);
+						e.target.value = "";
+					})
+					.catch((e) => {
+						alert(e.message);
+					});
+			} else {
+				alert("Please Enter A Valid Comment");
+			}
+		}
+	});
+
+	commentHolder.addEventListener("keypress", function (e) {
+		if (commentHolder.hasChildNodes(e.target)) {
+			if (e.key === "Enter") {
+				let commentId = e.target.dataset.comment;
+				let user = reply.dataset.user;
+				console.log(user);
+				let value = e.target.value;
+
+				if (value) {
+					let data = {
+						body: value,
+					};
+					let req = generateRequest(`/api/comments/replies/${commentId}`, "POST", data);
+					fetch(req)
+						.then((res) => res.json())
+						.then((data) => {
+							let replyElement = createReplyElement(data, user);
+							let parent = e.target.parentElement;
+							parent.previousElementSibling.appendChild(replyElement);
+							e.target.value = "";
+						})
+						.catch((e) => {
+							alert(e.message);
+						});
+				} else {
+					alert("Please Enter A Valid Reply");
+				}
+			}
+		}
+	});
+
+	likeBtn.addEventListener("click", function (e) {
+		let postId = likeBtn.dataset.react;
+		reqLikeDislike("like", postId)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				let likeText = data.liked ? "🧡 Liked" : "🧡 Like";
+				likeText = likeText + ` ( ${data.totalLikes} )`;
+				let dislikeText = `💔 Dislike ( ${data.totalDislikes} )`;
+
+				likeBtn.innerHTML = likeText;
+				dislikeBtn.innerHTML = dislikeText;
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	});
+
+	dislikeBtn.addEventListener("click", function (e) {
+		let postId = likeBtn.dataset.react;
+		reqLikeDislike("dislike", postId)
+			.then((res) => res.json())
+			.then((data) => {
+				let dislikeText = data.disliked ? "💔 Disliked" : "💔 Dislike";
+				dislikeText = dislikeText + ` ( ${data.totalDislikes} )`;
+				let likeText = `🧡 Like ( ${data.totalLikes} )`;
+
+				likeBtn.innerHTML = likeText;
+				dislikeBtn.innerHTML = dislikeText;
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	});
+};
+
+function generateRequest(url, method, body) {
+	let headers = new Headers();
+	headers.append("Accept", "Application/JSON");
+	headers.append("Content-Type", "Application/JSON");
+
+	let req = new Request(url, {
+		method,
+		headers,
+		body: JSON.stringify(body),
+		mode: "cors",
+	});
+
+	return req;
+}
+
+function createComment(comment) {
+	let innerHTML = `
+	<a href="/author/${comment.user._id}" class="sm-details">
+    	<img style="width:49px; height:49px" src="${comment.user.profilePics}" class="rounded-circle mx-3 my-3">
+    </a>
+	<div class="media-body w-100 pe-3 my-3">
+		<a class="d-flex justify-content-between w-100" href="/author/${comment.user._id}">
+			<span>${comment.user.username}</span>
+			<span class="text-muted">a few seconds ago</span>
+		</a>
+        <p style="width: 100%">${comment.body}</p>
+        <div class="my-3 reply-base">
+            <input class="form-control" type="text" placeholder="Press Enter to Reply" name="reply" data-comment=${comment._id} />
+        </div>
+    </div>
+    `;
+	let div = document.createElement("div");
+	div.className = "media border w-100";
+	div.innerHTML = innerHTML;
+
+	return div;
+}
+
+function createReplyElement(reply, user) {
+	let innerHTML = `
+		<a href="/author/${reply.user}">
+			<img src="${reply.profilePics}" class="align-self-start rounded-circle" style="width: 44px; height: 44px" alt="rep_profile">
+		</a>
+		<div class="media-body">
+			<a class="w-100 d-flex justify-content-between" href="/author/${reply.user}">
+				<span>${user}</span>
+				<span class="text-muted d-flex">
+					a few seconds ago
+				</span>
+			</a>
+			<p>${reply.body}</p>
+		</div>
+    `;
+
+	let div = document.createElement("div");
+	div.className = "media mt-3 w-100 gap-14";
+	div.innerHTML = innerHTML;
+
+	return div;
+}
+
+function reqLikeDislike(type, postId) {
+	console.log(postId);
+	let headers = new Headers();
+	headers.append("Accept", "Application/JSON");
+	headers.append("Content-Type", "Application/JSON");
+
+	let req = new Request(`/api/${type}/${postId}`, {
+		method: "GET",
+		headers,
+		mode: "cors",
+	});
+
+	return fetch(req);
+}
