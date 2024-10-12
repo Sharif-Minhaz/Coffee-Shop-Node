6ffdc656ade71534e684e637c791c9d15648a219
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-10-12 18:57:07 +0600

    style: add animations

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -15,27 +15,31 @@ let all = document.querySelectorAll("[data-category]");
 let menu = document.querySelectorAll("[data-category='menu']");
 let product = document.querySelectorAll("[data-category='product']");
 let filter = document.getElementsByClassName("filter");
+
 // all items
-filter[0].addEventListener("click", function () {
-	activeDeactive(0, 1, 2);
-	returningItems();
-});
-// only menu
-filter[1].addEventListener("click", function () {
-	activeDeactive(1, 0, 2);
-	returningItems();
-	for (let i = 0; i < product.length; i++) {
-		product[i].style.display = "none";
-	}
-});
-// only product
-filter[2].addEventListener("click", function () {
-	activeDeactive(2, 0, 1);
-	returningItems();
-	for (let i = 0; i < menu.length; i++) {
-		menu[i].style.display = "none";
-	}
-});
+if (filter?.length > 0) {
+	filter[0].addEventListener("click", function () {
+		activeDeactive(0, 1, 2);
+		returningItems();
+	});
+	// only menu
+	filter[1].addEventListener("click", function () {
+		activeDeactive(1, 0, 2);
+		returningItems();
+		for (let i = 0; i < product.length; i++) {
+			product[i].style.display = "none";
+		}
+	});
+	// only product
+	filter[2].addEventListener("click", function () {
+		activeDeactive(2, 0, 1);
+		returningItems();
+		for (let i = 0; i < menu.length; i++) {
+			menu[i].style.display = "none";
+		}
+	});
+}
+
 // returning all products and items
 function returningItems() {
 	for (let i = 0; i < all.length; i++) {
